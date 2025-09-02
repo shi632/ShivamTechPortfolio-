3480ecd9a40aef0b6b7843a5cb8fd7ae7bfaed93
shivamkumarshrma <shivamsharma79005@gmail.com>
2025-09-02 20:58:56 +0530

    first commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Web Developer & Tech Creator
+            </h3>
+
+            <p className="text-muted-foreground">
+              I am a Java full Stack Developer student skilled in java, SpringBoot, React.js and MySQL.
+              I enjoy building resposnive web applications and continuosly learning new technologies
+              to grow  a developer.
+            </p>
+
+            <p className="text-muted-foreground">
+              I'm passionate about creating elegant solutions to complex
+              problems, and I'm constantly learning new technologies and
+              techniques to stay at the forefront of the ever-evolving web
+              landscape.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="/Shivam.pdf"
+                target="_blank"
+                rel="noorpen noreferrer"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating responsive websites and web applications with
+                    modern frameworks.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Backend Development</h4>
+                  <p className="text-muted-foreground">
+                    Building robust REST APIs and server-side application using java,
+                    Spring Boot.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Database Management</h4>
+                  <p className="text-muted-foreground">
+                    Designing and managing relational databases using MySQL and
+                    handling data efficiently.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
